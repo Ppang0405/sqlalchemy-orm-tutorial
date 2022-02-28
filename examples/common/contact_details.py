6d008cd2d504d61ac0098ab6d8a6ed3e1528c7c8
Your Name <you@example.com>
2022-02-28 08:41:36 +0000

    update starter code: I did it

---
+++ examples/common/contact_details.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+from base import Base
+
+
+class ContactDetails(Base):
+    __tablename__ = 'contact_details'
+
+    id = Column(Integer, primary_key=True)
+    phone_number = Column(String)
+    address = Column(String)
+    # many-to-one | many contact details can be just relied one actor
+    actor_id = Column(Integer, ForeignKey('actors.id'))
+    actor = relationship("Actor", backref="contact_details")
+
+    def __init__(self, phone_number, address, actor):
+        self.phone_number = phone_number
+        self.address = address
+        self.actor = actor
+        
+        
